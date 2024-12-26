4ba4c7439b11dde99a0692891a8832cebc45df26
Penguin1110 <106901954+Penguin1110@users.noreply.github.com>
2024-12-26 21:54:54 +0800

    123

--- penguin.js
+++ penguin.js
@@ -1,13 +1,13 @@
 function navigateTo(pageId) {
-  // 獲取所有頁面
+ 
   const pages = document.querySelectorAll('.page');
   
-  // 確保頁面切換時隱藏所有頁面
+ 
   pages.forEach((page) => {
     page.classList.remove('active');
   });
 
-  // 顯示指定頁面
+
   const targetPage = document.getElementById(pageId);
   if (targetPage) {
     targetPage.classList.add('active');
